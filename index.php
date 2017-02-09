7c8505fede922e742119463565ba4be5718101de
Alexis <adoualle@gmail.com>
2017-02-09 11:29:59 +0100

    changement css du login et ajout de l'heure de vente

--- index.php
+++ index.php
@@ -7,15 +7,16 @@
     <title>Login Softbar</title>
     <link rel="stylesheet" href="css/style.css">
   </head>
-  <body>
-    <h1>Bienvenue</h1>
-      <div class="corps">
+  <body style="padding: 8% 0 0">
+
+      <div class="login">
+        <span><h2>Caisse</h2></span>
         <form class="" action="" method="post">
-          <label for="">Utilisateur: </label>
-          <input type="text" name="utilisateur" placeholder="nom d'utilisateur">
-          <label for="">Mot de passe: </label>
-          <input type="password" name="password" placeholder="*****">
-          <input type="submit" name="submit" value="Valider"><br>
+          <label for=""></label>
+          <input type="text" name="utilisateur" placeholder="Utilisateur"><br>
+          <label for=""></label>
+          <input type="password" name="password" placeholder="Mot de passe"><br>
+          <input type="submit" name="submit" value="Valider" class="btn_submit"><br>
           <span style="color:red"><?php echo $error ?></span>
 
         </form>
